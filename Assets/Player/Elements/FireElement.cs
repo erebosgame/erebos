e41397848c7e69553cc13ea460a336f163e2d94f
Roberto Pettinau <roberto.pettinau99@gmail.com>
2021-03-05 22:16:51 +0100

    Add melee attack & enemy targeting

--- Assets/Player/Elements/FireElement.cs
+++ Assets/Player/Elements/FireElement.cs
@@ -42,12 +42,14 @@ class FireElement : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Vector3 direction = (other.transform.position - Player.gameObject.transform.position).normalized;
-        if (System.Math.Abs(Vector3.Angle(Player.gameObject.transform.forward, direction)) > 90)
-            return;
             
         if (other.tag == "Enemy")
         {
+            Vector3 direction = (other.transform.position - Player.gameObject.transform.position).normalized;
+            print(System.Math.Abs(Vector3.Angle(Player.gameObject.transform.forward, direction)));
+            if (System.Math.Abs(Vector3.Angle(Player.gameObject.transform.forward, direction)) > 120)
+                return;
+            print("test");
             Enemy enemy= other.GetComponent<Enemy>();
             GameObject explosion = UnityEngine.Object.Instantiate(explosionPrefab);
             // GameObject enemyFire = UnityEngine.Object.Instantiate(fireballPrefab, other.transform);
---
+++ Assets/Player/MeleeAttack.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+    List<Collider> triggerList = new List<Collider>();
+    GameObject selected;
+    Enemy selectedEnemy;
+    Outline selectedOutline;
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Enemy")
+            return;
+
+        if(!triggerList.Contains(other))
+        {
+            triggerList.Add(other);
+        }
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        if(triggerList.Contains(other))
+        {
+            triggerList.Remove(other);
+        }
+    }
+
+    void GetAimed()
+    {
+        if (selected)
+        {
+            selectedOutline.OutlineWidth = 0;
+            selected = null;
+    }
+        List<(float,Collider)> angles = new List<(float, Collider)>();
+
+        foreach (Collider collider in triggerList) {
+            if(collider)
+                angles.Add((GetColliderAngle(collider),collider));
+        }         
+
+        angles.Sort((x,y) => x.Item1.CompareTo(y.Item1));
+
+        if (angles.Count > 0) 
+        {
+            if (angles[0].Item1 < 80)
+            {
+                selected = angles[0].Item2.gameObject;
+                selectedOutline = selected.GetComponent<Outline>();
+                selectedOutline.OutlineWidth = 3;
+                selectedEnemy = selected.GetComponent<Enemy>();
+            }
+        }        
+    }
+
+    void Update() 
+    {
+        GetAimed();
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (selected)
+                selectedEnemy.TakeDamage(3);
+        }    
+    }
+
+    float GetColliderAngle(Collider collider)
+    {
+        Vector3 cameraPos = Camera.main.transform.position;
+        Vector3 playerPos = Player.gameObject.transform.position;
+        Vector3 enemyPos = collider.gameObject.transform.position;
+
+        Vector3 playerAngle = (playerPos-cameraPos).normalized;
+        Vector3 enemyAngle = (enemyPos-playerPos).normalized;
+
+        return Vector3.Angle(playerAngle,enemyAngle);
+    }
+
+}
